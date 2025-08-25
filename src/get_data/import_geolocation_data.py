2f77e566e1aebacc58dabfba4fbc7497e5778af2
Roger J. Campbell <the_rfc@hotmail.co.uk>
2025-08-25 19:27:04 +0100

    Pathway to create cache working tested. Need to push geolocation data into bronze dir now

--- src/get_data/import_geolocation_data.py
+++ src/get_data/import_geolocation_data.py
@@ -1,6 +1,23 @@
 import pandas as pd
 from pathlib import Path
 
+def fetch_geolocation_data(
+        cache_folder: Path = Path("data/geolocation"),
+        filename: str = "geolocation.csv"        
+) -> Path:
+    
+    # Ensure cache directory exists
+    cache_folder = Path(cache_folder)
+    cache_folder.mkdir(parents=True, exist_ok=True)
+
+    # Compute full output path
+    output_path = cache_folder / filename
+
+    if output_path.exists():
+        return output_path
+
+
 check = "OK"
 if __name__ == "__main__":
     print(check)
+    fetch_geolocation_data()
