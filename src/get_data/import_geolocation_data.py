4168e5c11de4fd09d2e669981eeeb35e8d966762
Roger J. Campbell <the_rfc@hotmail.co.uk>
2025-08-25 19:42:55 +0100

    geolocation data successfully imported

--- src/get_data/import_geolocation_data.py
+++ src/get_data/import_geolocation_data.py
@@ -13,9 +13,34 @@ def fetch_geolocation_data(
     # Compute full output path
     output_path = cache_folder / filename
 
+    # Return existing cache if present
     if output_path.exists():
         return output_path
+    
+    # Download the countries CSV
+    url = (
+        "https://raw.githubusercontent.com/google/dspl/master/"
+        "samples/google/canonical/countries.csv"
+    )
 
+    try:
+        df = pd.read_csv(url)
+        print("Stored CSV as Pandas DataFrame:")
+        print(df.head())
+    except Exception as e:
+        raise RuntimeError(f"Failed to download geolocation data: {e}")
+    
+    df = df.rename(
+        columns={
+            "name":"country_name"
+        }
+    )
+    df = df[["country", "country_name", "latitude", "longitude"]]
+    print("Filtered to relevant fields")
+
+    df.to_csv(output_path, index=False)
+    print(f"Geolocation data saved to 'data/geolocation' folder")
+    return output_path
 
 check = "OK"
 if __name__ == "__main__":
