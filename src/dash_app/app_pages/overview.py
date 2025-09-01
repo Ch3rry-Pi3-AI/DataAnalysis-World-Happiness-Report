bb7f4a9a2da80cc376f3641d5f9ffe4b6080a7ce
Roger J. Campbell <the_rfc@hotmail.co.uk>
2025-09-01 15:45:47 +0100

    row and column list test implemented and tested

--- src/dash_app/app_pages/overview.py
+++ src/dash_app/app_pages/overview.py
@@ -8,10 +8,22 @@ dash.register_page(__name__, path="/overview", name="Overview", order=1)
 def _df() -> pd.DataFrame:
     return get_gold_df()
 
+def _summary_items(df: pd.DataFrame):
+    n_rows = len(df)
+    n_cols = df.shape[1]
+
+    items = [
+        html.Li([html.B("Rows: "), (f"{n_rows}")]),
+        html.Li([html.B("Columns:" ), (f"{n_cols}")])
+    ]
+
+    return items
+
 _df0 = _df()
 
 layout = html.Div(
     [
-        html.H2("World Happiness - Overview", className="fw-bold text-center mt-3")
+        html.H2("World Happiness - Overview", className="fw-bold text-center mt-3"),
+        html.Ul(_summary_items(_df0))
     ]
 )
