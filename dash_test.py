d00111440193075cb0f6743bbc9155d5b276c841
Roger J. Campbell <the_rfc@hotmail.co.uk>
2025-09-01 14:40:21 +0100

    data access module complete. tested in dash_test.py module. Can import the gold data.

--- dash_test.py
+++ dash_test.py
@@ -1,9 +1,10 @@
 from src import load_gold_happiness_data
-from src.dash_app.data_access import set_gold_df
+from src.dash_app.data_access import set_gold_df, get_gold_df
 from src.dash_app.create_app import dashboard
 
 if __name__ == "__main__":
-    gold_df = load_gold_happiness_data(verbose=True)
+    gold_df = load_gold_happiness_data(verbose=False)
     set_gold_df(gold_df)
+    print(get_gold_df().head())
     app = dashboard(enable_pages=True)
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
